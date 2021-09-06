a5fab52a933481fbf6fe00ada62c0f168eb1508c
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-06 02:14:10 -0400

    Added new configuration lines on nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,21 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap" }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/vendor/font-awesome/css/fontawesome-all.min.css',
+    '~/assets/css/font-electro.css',
+    '~/assets/vendor/animate.css/animate.min.css',
+    '~/assets/vendor/hs-megamenu/src/hs.megamenu.css',
+    '~/assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css',
+    '~/assets/vendor/fancybox/jquery.fancybox.css',
+    '~/assets/vendor/slick-carousel/slick/slick.css',
+    '~/assets/vendor/bootstrap-select/dist/css/bootstrap-select.min.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
