0c3cf38c153db7b4a643a3d68194737465ec180c
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-08 00:44:32 -0400

    Added login.vue with example of login in API of BuyAllTogether using axios

---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Login</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+    }
+  },
+  created () {
+    const userData = {
+      email: 'roldandvg@gmail.com',
+      password: '12345678'
+    }
+    const header = {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Language': 'en'
+      }
+    }
+    axios.post('https://admin.buyalltogether.tk/api/v1/login', userData, header)
+      .then((response) => {
+        console.log(response)
+        console.log('Success')
+      })
+      .catch((error) => {
+        console.log(error)
+        console.log('Error')
+      })
+  }
+}
+</script>
