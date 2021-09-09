47168d9b7913cc2de3cd08eee0ea380bbeda017d
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-08 21:26:03 -0400

    Added login form in login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Login</h1>
+    <form method="post" @submit.prevent="login">
+      <label class="label">Email</label>
+      <div class="control">
+        <input
+          v-model="email"
+          type="email"
+          name="email"
+        />
+      </div>
+      <label class="label">Password</label>
+      <div class="control">
+        <input
+          v-model="password"
+          type="password"
+          name="password"
+        />
+      </div>
+      <div class="control mt-2">
+        <button type="submit" class="button">
+          Log In
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
