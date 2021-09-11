463c905cb30c2d40e20c21938d1a6330b713fc94
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-11 13:31:56 -0400

    Added pages for testing

---
+++ pages/countries.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h1>Countries</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      countries: []
+    }
+  },
+  head: {
+    title: 'Countries'
+  },
+  created () {
+    const userData = {
+      store: 'true'
+    }
+    const header = {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Language': 'en'
+      }
+    }
+    axios.get('https://admin.buyalltogether.tk/api/v1/list/countries', userData, header)
+      .then((response) => {
+        this.countries = response.data.data
+        console.log(this.countries)
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
+}
+</script>
---
+++ pages/profile_types.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h1>Profile types</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      tipos: []
+    }
+  },
+  head: {
+    title: 'Profile types'
+  },
+  created () {
+    const userData = {
+      store: 'true'
+    }
+    const header = {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Language': 'en'
+      }
+    }
+    axios.post('https://admin.buyalltogether.tk/api/v1/list/profile/types', userData, header)
+      .then((response) => {
+        this.tipos = response.data.data
+        console.log(this.tipos)
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
+}
+</script>
