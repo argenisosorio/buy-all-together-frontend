f0d677c03787755140db259b698ed2665bf53e12
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-11 13:52:06 -0400

    Added countries data on input select

--- pages/register.vue
+++ pages/register.vue
@@ -15,7 +15,7 @@
         <label class="label">Profile type</label>
         <select v-model="profile_type_id" name="profile_type_id">
           <option
-            v-for="(x, index) in tipos"
+            v-for="(x, index) in profile_types"
             :key="index"
             :value="x.id"
           >
@@ -38,7 +38,15 @@
       <h2>Store</h2>
       <div>
         <label class="label">Country</label>
-        <input v-model="country_id" type="text" name="country_id">
+        <select v-model="country_id" name="country_id">
+          <option
+            v-for="(x, index) in countries"
+            :key="index"
+            :value="x.id"
+          >
+            {{ x.name }}
+          </option>
+        </select>
       </div>
       <div>
         <label class="label">Store name</label>
@@ -74,7 +82,8 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      tipos: [],
+      profile_types: [],
+      countries: [],
       name: '',
       email: '',
       password: '',
@@ -105,11 +114,17 @@ export default {
     }
     axios.post('https://admin.buyalltogether.tk/api/v1/list/profile/types', userData, header)
       .then((response) => {
-        // console.log(response)
-        this.tipos = response.data.data
+        this.profile_types = response.data.data
       })
       .catch((error) => {
-        alert(error)
+        console.log(error)
+      })
+    axios.get('https://admin.buyalltogether.tk/api/v1/list/countries', userData, header)
+      .then((response) => {
+        this.countries = response.data.data
+      })
+      .catch((error) => {
+        console.log(error)
       })
   },
   methods: {
