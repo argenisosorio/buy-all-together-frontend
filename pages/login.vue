847157420fdc8fad77343ef8e74f3274ae5ce496
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-19 21:18:28 -0400

    Adjustments in login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -8,44 +8,46 @@
           <div class="container min-height-420 overflow-hidden">
             <div
               class=""
-              data-pagi-classes="text-center position-absolute right-0 bottom-0 left-0 justify-content-start mb-3 mb-md-4 offset-xl-3 pl-2 pb-1">
+              data-pagi-classes="text-center position-absolute right-0 bottom-0 left-0 justify-content-start mb-3 mb-md-4 offset-xl-3 pl-2 pb-1"
+            >
               <div class="js-slide bg-img-hero-center">
                 <div class="row min-height-420 py-7 py-md-0">
                   <div class="offset-xl-3 col-xl-4 col-6 mt-md-8">
                     <h1>Log in</h1>
-                      <form method="post">
-                        <div class="form-group">
-                          <label class="form-label">Email</label>
-                          <input
-                            v-model="email"
-                            type="email"
-                            class="form-control"
-                            name="email"
-                            placeholder="Email"
-                            aria-label="Email"
-                            required
-                          >
-                        </div>
-                        <div class="form-group">
-                          <label class="form-label">Password</label>
-                          <input
-                            v-model="password"
-                            type="password"
-                            class="form-control"
-                            name="password"
-                            placeholder="Password"
-                            aria-label="Password"
-                            required
-                          >
-                        </div>
-                        <div>
-                          <button
-                            type="submit"
-                            @click.prevent="login()"
-                            class="btn btn-block btn-sm btn-primary transition-3d-hover">
-                            Log in
-                          </button>
-                        </div>
+                    <form method="post">
+                      <div class="form-group">
+                        <label class="form-label">Email</label>
+                        <input
+                          v-model="email"
+                          type="email"
+                          class="form-control"
+                          name="email"
+                          placeholder="Email"
+                          aria-label="Email"
+                          required
+                        >
+                      </div>
+                      <div class="form-group">
+                        <label class="form-label">Password</label>
+                        <input
+                          v-model="password"
+                          type="password"
+                          class="form-control"
+                          name="password"
+                          placeholder="Password"
+                          aria-label="Password"
+                          required
+                        >
+                      </div>
+                      <div>
+                        <button
+                          type="submit"
+                          class="btn btn-block btn-sm btn-primary transition-3d-hover"
+                          @click.prevent="login()"
+                        >
+                          Log in
+                        </button>
+                      </div>
                     </form>
                   </div>
                 </div>
